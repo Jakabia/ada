9f3dd91c9c7559df682d8fb67fac73d07ba49c62
Jakabia <gezumacht@gmail.com>
2018-12-10 15:37:06 +0100

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+d = {'A': [1, 2, 3, 4], 'B': [3, 4, 5, 6]}
+df = pd.DataFrame(data=d)
+
+
+# In[6]:
+
+
+df['mean'] = (df['A'] + df['B']) / 2
+
+
+# In[9]:
+
+
+df.describe()
+
